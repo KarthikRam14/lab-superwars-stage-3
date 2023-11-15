f7797141f2425cb2765dea08af7932e777ad4124
KarthikRam14 <karthikram1441@gmail.com>
2023-11-15 16:43:54 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -23,28 +23,34 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
 
     // Instead of forloop use Map method
     // Code here
-
+    const detailedPlayers= PLAYERS.map((player, index) => ({
+        name:player,
+        image:`images/super-${index+1}.png`,
+        strength:getRandomStrength(),
+        type: index % 2 === 0 ? "hero":"villain",
+    }))
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
+    return Math.floor(Math.random() * 100);
 }
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
-    return fragment;
+    const fragment = players.filter(player => player.type === type).map(player => `<div class="player"> 
+            <img src="${player.image}">
+            <p>${player.name}</p>
+            <h1>${player.strength}</h1>
+            </div>`).join('');//this para filters and creates a new array for elements which satisfy the condition
+    return fragment;//then the formed array is mapped and a new array is created which mathes with the html and is joined by a space.
 }
 
 // Display players in HTML
